640e831545edbfcb3b1fe5e7f28936c51bb311d0
Chinonso Chukwuogor <nonsochukwuogor@gmail.com>
2023-03-19 16:54:57 -0700

    literals

--- src/pages/content/components/literal.tsx
+++ src/pages/content/components/literal.tsx
@@ -1,11 +1,38 @@
-import { CommonValueProps } from "../types"
+import { CommonValueProps } from "../types";
 import classnames from "classnames/bind";
 import styles from "./literal.module.css";
+import { LiteralNode } from "json-to-ast";
 const cx = classnames.bind(styles);
 
-type LiteralComponentProps =  CommonValueProps & {
-  node: jsonToAst.LiteralNode
+type LiteralComponentProps = CommonValueProps & {
+  node: jsonToAst.LiteralNode;
+};
+export function LiteralComponent(props: LiteralComponentProps) {
+  switch (true) {
+    case typeof props.node.value === "number":
+      return <span className={cx("number-value")}>{props.node.raw}</span>;
+    case typeof props.node.value === "string":
+      return getStringElement(props.node);
+    case typeof props.node.value === "boolean":
+      return <span className={cx("boolean-value")}>{props.node.raw}</span>;
+    case props.node.value === null:
+      return <span className={cx("null-value")}>{props.node.raw}</span>;
+    default:
+      return <span className={cx("undefined-value")}>{props.node.raw}</span>;
+  }
 }
-export function LiteralComponent(props: LiteralComponentProps){
-  return <span className={cx('string-value')}>{props.node.raw}</span>
+
+function getStringElement(node: LiteralNode) {
+  if (isUrl(node.value as string)) {
+    return (
+      <a href={node.value as string} className={cx("url-value")}>
+        {node.raw}
+      </a>
+    );
+  }
+  return <span className={cx("string-value")}>{node.raw}</span>;
+}
+const urlRegex = /^(((https?:)?\/\/?)|#).+/;
+function isUrl(value: string) {
+    return urlRegex.test(value);
 }
