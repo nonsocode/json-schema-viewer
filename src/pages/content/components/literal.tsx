3463799156cc4846c26b42ff518c900f04742627
Chinonso Chukwuogor <nonsochukwuogor@gmail.com>
2023-03-20 12:53:24 -0700

    Instantiate on text docs too

--- src/pages/content/components/literal.tsx
+++ src/pages/content/components/literal.tsx
@@ -3,6 +3,8 @@ import classnames from "classnames/bind";
 import styles from "./literal.module.css";
 import { LiteralNode } from "json-to-ast";
 import { isUrl } from "@src/utils/url";
+import { useContext, useMemo } from "react";
+import { UrlContext } from "../context/url";
 const cx = classnames.bind(styles);
 
 type LiteralComponentProps = CommonValueProps & {
@@ -13,7 +15,7 @@ export function LiteralComponent(props: LiteralComponentProps) {
     case typeof props.node.value === "number":
       return <span className={cx("number-value")}>{props.node.raw}</span>;
     case typeof props.node.value === "string":
-      return getStringElement(props.node);
+      return <StringComponent node={props.node} />;
     case typeof props.node.value === "boolean":
       return <span className={cx("boolean-value")}>{props.node.raw}</span>;
     case props.node.value === null:
@@ -22,14 +24,29 @@ export function LiteralComponent(props: LiteralComponentProps) {
       return <span className={cx("undefined-value")}>{props.node.raw}</span>;
   }
 }
-
-function getStringElement(node: LiteralNode) {
-  if (isUrl(node.value as string)) {
-    return (
-      <a href={node.value as string} className={cx("url-value")}>
-        {node.raw}
-      </a>
-    );
-  }
-  return <span className={cx("string-value")}>{node.raw}</span>;
+type StringComponentProps = {
+  node: LiteralNode;
+};
+function StringComponent(props: StringComponentProps) {
+  const urlContext = useContext(UrlContext);
+  const url = useMemo(() => {
+    if (!isUrl(props.node.value as string)) return null;
+    let url: URL;
+    const partialUrl = props.node.value as string;
+    if (partialUrl.startsWith("#")) {
+      url = new URL(urlContext.fullPath);
+      url.hash = partialUrl;
+    } else {
+      url = new URL(partialUrl);
+    }
+    return url;
+  }, [urlContext.fullPath, props.node.value]);
+  
+  return url ? (
+    <a href={url.toString()} className={cx("url-value")}>
+      {props.node.raw}
+    </a>
+  ) : (
+    <span className={cx("string-value")}>{props.node.raw}</span>
+  );
 }
--- src/pages/content/index.ts
+++ src/pages/content/index.ts
@@ -1,6 +1,10 @@
 
 function isJsonDocument() {
-  return document.contentType === "application/json";
+  if (document.contentType === "application/json") return true;
+  if (document.contentType === "application/vnd.api+json") return true;
+  if (document.contentType === "application/schema+json") return true;
+  if (document.contentType === "application/ld+json") return true;
+  if (document.contentType === "text/plain" && document.body.querySelector(":scope > pre")) return true;
 }
 import('./update')
 isJsonDocument() &&
